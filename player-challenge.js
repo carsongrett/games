684d107c3480dec9c6f08689d44c5c19c4a6da3e
carsongrett <carsongrett1@gmail.com>
2025-07-02 22:32:37 -0500

    Add files via upload

--- player-challenge.js
+++ player-challenge.js
@@ -12,7 +12,7 @@ class PlayerChallengeGame {
     async loadPlayers() {
         try {
             console.log('Starting to load NFL players...'); // Debug log
-            const response = await fetch('nfl_players.csv');
+            const response = await fetch('nfl_players.csv?v=4');
             
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
